4df8cd39b6b9c52f2174975a83f77e54379423ba
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-05-30 13:50:29 +0100

    updated views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets ,status , filters
 from .models import Users, Message , Conversation
 from .serializers import Users, MessageSerializer, ConversationSerializer
 
@@ -7,9 +7,14 @@ from .serializers import Users, MessageSerializer, ConversationSerializer
 class ConservationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    earch_fields = ['participants_username']
+
      
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body']
 
