148915808407efbc882d70a05ba25004611b92c1
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-05-30 13:29:15 +0100

    second commit

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,40 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid 
+
  
 # Create your models here.
 class Users(AbstractUser):
     '''Model representing a chat in the messaging app.'''
-    Name_of_user = models.CharField(max_length=100, unique=True)
-    Email_of_user = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=100, unique=True)
-    is_online = models.EmailField(max_length=100, unique=True)
-    bio = models.TextField(blank= True , null= True)
-    user_profile = models.ImageField(upload_to='profile/', blank= True , null = True)
+    email = models.CharField(max_length=255, unique=True)
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    primary_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.username
     
 
-class Conversation (models.Model):
+class conversation (models.Model):
     '''model representing a conversation in the messaging app.'''
-    participant1 = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='participant1')
-    participant2 = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='participant2')
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
+    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    participant = models.ManyToManyField(Users, related_name='participants')
 
 
     def __str__(self):
-        return f"Conversation between {self.participant1.Name_of_user} and {self.participant2.Name_of_user} created at {self.created_at}"
+        return f"Conversation between {self.participant.all()} with ID {self.conversation_id}"
 
 
-class Message(models.Model):
+class message(models.Model):
     '''model representing a message in the messaging app.'''
-    sender = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='sender')
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='conversation')
-    text = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()
 
     def __str__(self):
-        return f"Message from {self.sender.Name_of_user} in conversation {self.conversation.id} at {self.timestamp}: {self.text[:20]}..."
+        return f"Message {self.message_id} sent at {self.sent_at} with body: {self.message_body}"
