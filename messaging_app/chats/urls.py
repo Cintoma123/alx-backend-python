da9414ba8510e0de148130804e4776ac38bac67b
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-05-30 13:45:02 +0100

    updated urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedDefaultRouter  
-from .views import  MessageViewSet , ConservationViewSet
+from rest_framework_nested.routers import NestedDefaultRouter
+from .views import MessageViewSet, ConservationViewSet
 
 # Import your viewsets here
 # Ensure you have the correct import paths for your viewsets
 router = DefaultRouter()
-router.register(r'conversations', ConservationViewset)
+router.register(r'conversations', ConservationViewSet)
 router.register(r'messages', MessageViewSet, basename='Conversation')
 
 nested_router = NestedDefaultRouter(
-    router, r'Conversation', lookup='conversation'
+    router, r'conversations', lookup='conversation'
 )
 nested_router.register(
-    r'Message', MessageViewSet, basename='conversation-message'
+    r'messages', MessageViewSet, basename='conversation-message'
 )
 
 urlpatterns = [
